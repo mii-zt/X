13338f9eef38e33f13eb65560037ea662401fd7f
mii <korondog777@gmail.com>
2025-05-23 17:11:23 +0900

    Update X.py
    フォームを用いて投稿したらフォームの内容をリセットする

--- X.py
+++ X.py
@@ -7,21 +7,24 @@ if 'posts' not in st.session_state:
 
 st.title('X模倣版') #アプリのタイトル
 
-post=st.text_input('投稿内容の入力') #テキストの入力
-upload_file=st.file_uploader('画像を選択してください',type=['jpg','jpeg','png']) #画像の選択
+with st.form(key="post_form",clear_on_submit=True):
+    st.session_state.post_text=st.text_input('投稿内容の入力') #テキストの入力
+    st.session_state.post_image=st.file_uploader('画像を選択してください',type=['jpg','jpeg','png']) #画像の選択
+    submitted=st.form_submit_button('投稿')
 
 
 #投稿内容の表示と警告
-if st.button('投稿'):
-    if post:
+if submitted:
+    if st.session_state.post_text:
         st.success('投稿完了')
         image=None
-        if upload_file:
-            image=upload_file.getvalue()
+        if st.session_state.post_image:
+            image=st.session_state.post_image.getvalue()
         st.session_state.posts.append({ #同一のリストに文字と画像を保存
-            'text':post,
+            'text':st.session_state.post_text,
             'image':image
-            })    
+            })
+
     else:
         st.warning('投稿内容を入力してください')
 
