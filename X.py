25332c875d10990fc131c5486a8800c5690f39c5
mii <korondog777@gmail.com>
2025-05-23 16:56:58 +0900

    Update X.py
    画像の投稿と投稿を新しい順に表示する

--- X.py
+++ X.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from PIL import Image
+from io import BytesIO
 
 if 'posts' not in st.session_state:
-    st.session_state.posts=[]
-if 'images' not in st.session_state:
     st.session_state.posts=[]
 
 st.title('X模倣版') #アプリのタイトル
@@ -16,13 +15,19 @@ upload_file=st.file_uploader('画像を選択してください',type=['jpg','jp
 if st.button('投稿'):
     if post:
         st.success('投稿完了')
-        st.session_state.posts.append(post)
+        image=None
         if upload_file:
-            image=Image.open(upload_file)
-            st.session_state.posts.append(image)
-            st.image(image,caption="アップロードされた画像",use_column_width=True)
+            image=upload_file.getvalue()
+        st.session_state.posts.append({ #同一のリストに文字と画像を保存
+            'text':post,
+            'image':image
+            })    
     else:
         st.warning('投稿内容を入力してください')
 
-for post in st.session_state.posts:
-    st.text_area('投稿内容',post)
+
+for post in reversed(st.session_state.posts): #新しい順で表示
+    st.text_area('投稿内容',post['text'],height=100)
+    if post['image']:
+        image=Image.open(BytesIO(post['image']))
+        st.image(image,caption='投稿画像',use_column_width=True)
