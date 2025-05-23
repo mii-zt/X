2bda2fc7b4710dbd02140d164db8e9db27b5a88b
mii <korondog777@gmail.com>
2025-05-23 17:29:01 +0900

    Update X.py
    画像のみでの投稿を可能にする
    無駄なsessionの修正

--- X.py
+++ X.py
@@ -9,20 +9,20 @@ if 'posts' not in st.session_state:
 st.title('X模倣版') #アプリのタイトル
 
 with st.form(key="post_form",clear_on_submit=True):
-    st.session_state.post_text=st.text_input('投稿内容の入力') #テキストの入力
-    st.session_state.post_image=st.file_uploader('画像を選択してください',type=['jpg','jpeg','png']) #画像の選択
+    post_text=st.text_input('投稿内容の入力') #テキストの入力
+    post_image=st.file_uploader('画像を選択してください',type=['jpg','jpeg','png']) #画像の選択
     submitted=st.form_submit_button('投稿')
 
 
 #投稿内容の表示と警告
 if submitted:
-    if st.session_state.post_text:
+    if post_text or post_image:
         st.success('投稿完了')
         image=None
-        if st.session_state.post_image:
-            image=st.session_state.post_image.getvalue()
+        if post_image:
+            image=post_image.getvalue()
         st.session_state.posts.append({ #同一のリストに文字と画像を保存
-            'text':st.session_state.post_text,
+            'text':post_text,
             'image':image,
             'time':datetime.now().strftime('%Y-%m-%d %H:%M:%S') #投稿時間の記録
             })
@@ -32,8 +32,8 @@ if submitted:
 
 
 for post in reversed(st.session_state.posts): #新しい順で表示
-    
-    st.text_area('投稿内容',post['text'],height=100)
+    if post['text']:
+        st.text_area('投稿内容',post['text'],height=100)
     if post['image']:
         image=Image.open(BytesIO(post['image']))
         st.image(image,caption='投稿画像',use_column_width=True)
