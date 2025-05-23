29fc4d254f8a2f2cc3d36be3c45df7858c1a8a22
mii <korondog777@gmail.com>
2025-05-23 13:59:28 +0900

    Update X.py
    投稿の表示と警告

--- X.py
+++ X.py
@@ -4,5 +4,11 @@ st.title('X模倣版') #アプリのタイトル
 
 post=st.text_input('投稿内容の入力') #テキストの入力
 
-st.button('投稿')
+#投稿内容の表示と警告
+if st.button('投稿'):
+    if post:
+        st.success('投稿完了')
+        st.text_area('投稿内容',post)
+    else:
+        st.warning('投稿内容を入力してください')
 
